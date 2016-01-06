d9616e796bab6455c6eb79c6520247cb162719b7
damienbod <damien_bod@hotmail.com>
2016-01-06 14:42:28 +0100

    renaming provider for SQLite

--- src/DataAccessSqliteProvider/Properties/AssemblyInfo.cs
+++ src/DataAccessSqliteProvider/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataAccessSqlite")]
+[assembly: AssemblyTitle("DataAccessSqliteProvider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataAccessSqlite")]
+[assembly: AssemblyProduct("DataAccessSqliteProvider")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
